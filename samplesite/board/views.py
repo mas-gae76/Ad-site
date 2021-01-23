e4a193063289573a96f410fd266bd48a6a08ea3e
mas-gae76 <76181978+mas-gae76@users.noreply.github.com>
2021-01-23 23:07:15 +0300

    ads is shown

--- samplesite/board/views.py
+++ samplesite/board/views.py
@@ -1,9 +1,7 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 from .models import Board
 
 
 def index(request):
-    s = 'Список объвлений\r\n\r\n\r\n'
-    for b in Board.objects.order_by('-published'):
-        s += b.title + '\r\n' + b.content + '\r\n\r\n'
-    return HttpResponse(s, content_type='text/plain; charset=utf-8')
+    bs = Board.objects.order_by('-published')
+    return render(request, 'board/index.html', {'bs': bs})
