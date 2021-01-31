931c67d2960c891e8fcbf385fde2a02069c5d64c
mas-gae76 <76181978+mas-gae76@users.noreply.github.com>
2021-01-31 15:30:19 +0300

    compact paginator was added

--- samplesite/board/views.py
+++ samplesite/board/views.py
@@ -9,13 +9,20 @@ from django.core.paginator import Paginator
 def index(request):
     bs = Board.objects.all()
     rubrics = Rubric.objects.all()
-    paginator = Paginator(bs, 5)
+    paginator = Paginator(bs, 7)
+    num_pages = paginator.num_pages
     if 'page' in request.GET:
         page_num = request.GET['page']
     else:
         page_num = 1
     page = paginator.get_page(page_num)
-    context = {'bs': page.object_list, 'page': page, 'rubrics': rubrics}
+    if num_pages < 11 or page.number < 6:
+        pages = [x for x in range(1, min(num_pages + 1, 11))]
+    elif page.number > num_pages - 6:
+        pages = [x for x in range(num_pages - 10, num_pages + 1)]
+    else:
+        pages = [x for x in range(num_pages - 5, num_pages + 6)]
+    context = {'bs': page.object_list, 'page': page, 'rubrics': rubrics, 'pages': pages}
     return render(request, 'board/index.html', context)
 
 
@@ -23,13 +30,20 @@ def by_rubric(request, rubric_id):
     bs = Board.objects.filter(rubric=rubric_id)
     rubrics = Rubric.objects.all()
     current_rubric = Rubric.objects.get(pk=rubric_id)
-    paginator = Paginator(bs, 5)
+    paginator = Paginator(bs, 2)
+    num_pages = paginator.num_pages
     if 'page' in request.GET:
         page_num = request.GET['page']
     else:
         page_num = 1
     page = paginator.get_page(page_num)
-    context = {'bs': page.object_list, 'page': page,'rubrics': rubrics, 'current_rubric': current_rubric}
+    if num_pages < 11 or page.number < 6:
+        pages = [x for x in range(1, min(num_pages + 1, 11))]
+    elif page.number > num_pages - 6:
+        pages = [x for x in range(num_pages - 10, num_pages + 1)]
+    else:
+        pages = [x for x in range(num_pages - 5, num_pages + 6)]
+    context = {'bs': page.object_list, 'page': page, 'rubrics': rubrics, 'current_rubric': current_rubric, 'pages': pages}
     return render(request, 'board/by_rubric.html', context)
 
 
