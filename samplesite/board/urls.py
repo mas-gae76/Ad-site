c97400880853214dfe67aed6d4cfe358d95da76b
mas-gae76 <76181978+mas-gae76@users.noreply.github.com>
2021-02-01 23:51:31 +0300

    user posts template url

--- samplesite/board/urls.py
+++ samplesite/board/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
-from .views import index, by_rubric, BoardCreateView
+from django.urls import path, include
+from .views import (index, by_rubric, BoardCreateView, show_user_posts)
 
 
 urlpatterns = [
     path('add/', BoardCreateView.as_view(), name='add'),
     path('<int:rubric_id>/', by_rubric, name='by_rubric'),
     path('', index, name='index'),
+    path('user_posts/', show_user_posts, name='user_posts')
 ]
