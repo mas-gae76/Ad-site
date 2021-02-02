9fe47afdd57eb3e25cea22875a2e28cc5557c656
mas-gae76 <76181978+mas-gae76@users.noreply.github.com>
2021-02-02 19:11:30 +0300

    method of adding ads was changed

--- samplesite/board/urls.py
+++ samplesite/board/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path, include
-from .views import (index, by_rubric, BoardCreateView, show_user_posts)
+from django.urls import path
+from .views import (index, by_rubric, add_ad, show_user_posts)
 
 
 urlpatterns = [
-    path('add/', BoardCreateView.as_view(), name='add'),
+    path('add/', add_ad, name='add'),
     path('<int:rubric_id>/', by_rubric, name='by_rubric'),
     path('', index, name='index'),
     path('user_posts/', show_user_posts, name='user_posts')
