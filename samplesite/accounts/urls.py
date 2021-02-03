3273257657b14bb8fc003e1c477fd3fcf39156c8
mas-gae76 <76181978+mas-gae76@users.noreply.github.com>
2021-02-03 21:56:24 +0300

    urls for password reset was changed

--- samplesite/accounts/urls.py
+++ samplesite/accounts/urls.py
@@ -1,12 +1,17 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth.views import PasswordResetCompleteView, PasswordResetConfirmView
 from django.urls import path
 
 
 urlpatterns = [
-    url('change_password/', views.changePassword, name='change_password'),
+    url('change_password/', views.change_password, name='change_password'),
     url('logout/', views.logout_view, name='logout'),
     path('login/', views.user_login, name='login'),
     url('register/', views.register, name='register'),
-    url('profile/', views.show_person_cabinet, name='profile')
+    url('profile/', views.show_person_cabinet, name='profile'),
+    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('reset_password/', views.password_reset, name='reset_password'),
+    path('done/', views.password_reset_done, name='password_reset_done'),
 ]
