75cc98523df5b0e5d04b1da98bdb2a24228be1ea
mas-gae76 <76181978+mas-gae76@users.noreply.github.com>
2021-02-04 23:35:14 +0300

    added url to search

--- samplesite/board/urls.py
+++ samplesite/board/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import (index, by_rubric, add_ad, show_user_posts)
+from .views import (index, by_rubric, add_ad, show_user_posts, search)
 
 
 urlpatterns = [
     path('add/', add_ad, name='add'),
     path('<int:rubric_id>/', by_rubric, name='by_rubric'),
     path('', index, name='index'),
-    path('user_posts/', show_user_posts, name='user_posts')
+    path('user_posts/', show_user_posts, name='user_posts'),
+    path('search/', search, name='post_search')
 ]
