04e7ebc504b57f7f45e8823896aabdfbf87c8dc1
mas-gae76 <76181978+mas-gae76@users.noreply.github.com>
2021-02-04 23:31:09 +0300

    added search form

--- samplesite/board/forms.py
+++ samplesite/board/forms.py
@@ -7,3 +7,7 @@ class BoardForm(forms.ModelForm):
     class Meta:
         model = Board
         fields = ('title', 'content', 'price', 'rubric')
+
+
+class SearchForm(forms.Form):
+    keyword = forms.CharField(required=False, max_length=20, label='')
