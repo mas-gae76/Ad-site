cc8c6d5872de0e5c3c9f327fa87325e15719a04e
mas-gae76 <76181978+mas-gae76@users.noreply.github.com>
2021-02-04 19:02:16 +0300

    regiseter method wrapping

--- samplesite/accounts/urls.py
+++ samplesite/accounts/urls.py
@@ -2,13 +2,14 @@ from django.conf.urls import url
 from . import views
 from django.contrib.auth.views import PasswordResetCompleteView, PasswordResetConfirmView
 from django.urls import path
+from .decorators import check_recaptcha
 
 
 urlpatterns = [
     url('change_password/', views.change_password, name='change_password'),
     url('logout/', views.logout_view, name='logout'),
     path('login/', views.user_login, name='login'),
-    url('register/', views.register, name='register'),
+    url('register/', check_recaptcha(views.register), name='register'),
     url('profile/', views.show_person_cabinet, name='profile'),
     path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
