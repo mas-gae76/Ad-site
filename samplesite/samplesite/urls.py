1bc29775e72ada8346f107441eb1451056e74430
mas-gae76 <76181978+mas-gae76@users.noreply.github.com>
2021-02-07 14:59:59 +0300

    changed import image settings

--- samplesite/samplesite/urls.py
+++ samplesite/samplesite/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
-from django.conf import settings
+from .settings import MEDIA_URL, MEDIA_ROOT
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('board/', include('board.urls')),
     path('accounts/', include('accounts.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(MEDIA_URL, document_root=MEDIA_ROOT)
