21fc01255a9cb788a66bf8f8c1d46d8f882d0399
mas-gae76 <76181978+mas-gae76@users.noreply.github.com>
2021-02-07 14:53:42 +0300

    added fields to display at admin site

--- samplesite/board/admin.py
+++ samplesite/board/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
-from .models import Board, Rubric
+from .models import Board, Rubric, AdditionalImage
 
 # Register your models here.
 
 
+class AdditionalImageInline(admin.TabularInline):
+    model = AdditionalImage
+
+
 class BoardAdmin(admin.ModelAdmin):
-    list_display = ('user', 'title', 'content', 'price', 'published', 'rubric')
+    inlines = [AdditionalImageInline, ]
+    list_display = ('user', 'title', 'image', 'content', 'price', 'published', 'contacts', 'rubric', 'is_active')
+    fields = (('rubric', 'user'), 'title', 'content', 'price', 'contacts', 'image', 'is_active')
+
     list_display_links = ('title', 'content')
     search_fields = ('title', 'content')
 
