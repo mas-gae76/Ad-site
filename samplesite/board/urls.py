edb41e10edb0ea8e47bbf50d4cffeee905c293bd
mas-gae76 <76181978+mas-gae76@users.noreply.github.com>
2021-02-22 21:33:13 +0300

    added url to delete ad

--- samplesite/board/urls.py
+++ samplesite/board/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import (index, by_rubric, add_ad, show_user_posts, search)
+from . import views
 
 
 urlpatterns = [
-    path('add/', add_ad, name='add'),
-    path('<int:rubric_id>/', by_rubric, name='by_rubric'),
-    path('', index, name='index'),
-    path('user_posts/', show_user_posts, name='user_posts'),
-    path('search/', search, name='post_search')
+    path('add/', views.add_ad, name='add'),
+    path('<int:rubric_id>/', views.by_rubric, name='by_rubric'),
+    path('', views.index, name='index'),
+    path('user_posts/', views.show_user_posts, name='user_posts'),
+    path('search/', views.search, name='post_search'),
+    path('delete_ad/<int:ad_id>/', views.delete_ad, name='delete_ad'),
 ]
