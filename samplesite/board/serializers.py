7c25969d5cc3ab3507a512199c7b66b5a25917c5
mas-gae76 <76181978+mas-gae76@users.noreply.github.com>
2021-03-10 22:53:55 +0300

    modified serializer model

--- samplesite/board/serializers.py
+++ samplesite/board/serializers.py
@@ -2,15 +2,11 @@ from rest_framework import serializers
 from .models import Board
 
 
-class BoardSerializer(serializers.Serializer):
-    title = serializers.CharField(max_length=50)
-    image = serializers.ImageField()
-    content = serializers.CharField()
-    price = serializers.IntegerField()
-    contacts = serializers.CharField(max_length=15)
-    published = serializers.DateTimeField()
-    user = serializers.CharField()
-    rubric = serializers.CharField()
+class BoardSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Board
+        fields = ['title', 'content', 'price', 'contacts', 'published']
 
     def create(self, validated_data):
         return Board.objects.create(**validated_data)
