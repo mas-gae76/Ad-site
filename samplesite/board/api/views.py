d3dd84907eb6fe08e368ec27dd0e436ebae8a5e3
mas-gae76 <76181978+mas-gae76@users.noreply.github.com>
2021-03-25 22:09:35 +0300

    added permission to view content

--- samplesite/board/api/views.py
+++ samplesite/board/api/views.py
@@ -4,6 +4,7 @@ from .serializers import BoardSerializer
 from rest_framework.response import Response
 from rest_framework import viewsets
 from rest_framework.decorators import action
+from rest_framework.permissions import BasePermission
 
 
 class BoardListView(generics.ListAPIView):
@@ -25,3 +26,12 @@ class BoardViewSet(viewsets.ReadOnlyModelViewSet):
         board = self.get_object()
         board.save()
         return Response({'enrolled': True})
+
+    @action(methods=['get'], detail=True)
+    def contents(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+
+class IsEnrolled(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.filter(id=request.user.id).exists()
